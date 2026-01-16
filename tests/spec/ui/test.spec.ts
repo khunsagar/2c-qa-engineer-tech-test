e0b794ece9c38326bc622ef3a07b421a3ba34215
Sagar <51394096+khunsagar@users.noreply.github.com>
2026-01-16 10:24:33 +0530

    PR: updated folder naming

--- tests/spec/ui/test.spec.ts
+++
@@ -1,7 +0,0 @@
-import test, { expect } from "@playwright/test";
-
-test.describe('Landing page', () => {
-    test.skip('verify should load and siplay correct book title', async ({ page }) => {
-        await page.goto('http://localhost:3000');
-})
-})

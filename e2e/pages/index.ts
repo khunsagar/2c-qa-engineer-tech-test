8bbba88c57bbb9c64a4914ab48f63f4aba7c6c7c
Sagar <51394096+khunsagar@users.noreply.github.com>
2026-01-17 21:55:11 +0530

    feat(pages):updating index for pages

---
+++ e2e/pages/index.ts
@@ -0,0 +1,5 @@
+export { BasePage } from './base.page';
+export { HomePage } from './landing.page';
+export { AddBookPage, addBookPage } from './addbook.page';
+export { BookDetailsPage } from './bookdetails.page';
+export { BooksApiPage } from './books.api.page';

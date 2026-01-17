a95337e095cf4329b58a8a421fbad5f397ddba07
Sagar <51394096+khunsagar@users.noreply.github.com>
2026-01-18 00:19:27 +0530

    test(e2e):addbook updated labels

--- e2e/spec/ui/addBookPage.spec.ts
+++ e2e/spec/ui/addBookPage.spec.ts
@@ -15,14 +15,24 @@ test.describe('Add Book Page', () => {
         await addBookPage.verifyBookCreatedAndNavigated();
     });
 
+   // can be covered in unit test
     test('should show error for missing title', async () => {
         await addBookPage.fillAuthorOnly();
         await addBookPage.verifyTitleErrorMessage();
     });
-
+    // can be covered in unit test
     test('should show error for missing author', async () => {
         await addBookPage.fillTitleOnly();
         await addBookPage.verifyAuthorErrorMessage()
+    });
+    // can be covered in unit test
+    test('should show error for invalid  Published Year', async () => {
+        await addBookPage.verifyPublisherErrorMessage()
+    });
+
+      // can be covered in unit test
+     test('should show error for invalid pages values ', async () => {
+        await addBookPage.verifyPagesErrorMessage()
     });
 
     test('should navigate back to home page when customer clicks cancel', async () => {
@@ -34,4 +44,24 @@ test.describe('Add Book Page', () => {
         await addBookPage.clickBackToLibrary();
         await addBookPage.verifyOnHomePage();
     });
-});
+
+})
+   test.describe('unit tests', () => {
+    test.fixme('should throw an error when customer try to select null or blank Genre', async () => {
+     // TODO: Implement test
+    });
+     test.fixme('should select first genre option', async () => {
+        // TODO: Implement test
+    });
+     test.fixme('should select last genre option', async () => {
+        // TODO: Implement test
+    });
+    test.fixme('shoudl have charater limit for title field ', async () => {
+        // TODO: Implement test
+    });
+    test.fixme('should have character limit for author field ', async () => {
+        // TODO: Implement test
+    });
+    
+
+})
